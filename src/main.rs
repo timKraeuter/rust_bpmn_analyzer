7dfd932546f7a3d3bfdc3e2d16dbdc6ab503b266
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-10-23 22:59:49 +0200

    12.6

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use bpmnanalyzer::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = bpmnanalyzer::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
