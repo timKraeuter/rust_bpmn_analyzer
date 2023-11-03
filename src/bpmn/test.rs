65026c4088a706fb35941e4223264c6cc10720d7
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-11-03 20:24:13 +0100

    Move tests to its own module/file

---
+++ src/bpmn/test.rs
@@ -0,0 +1,139 @@
+#[cfg(test)]
+mod tests {
+    use crate::bpmn::{BPMNCollaboration, BPMNProcess, FlowNode, read_bpmn_file};
+    use crate::bpmn::state_space::{ProcessSnapshot, State};
+    use crate::Config;
+
+    #[test]
+    fn create_start_state() {
+        let collaboration = read_bpmn_file(&Config {
+            file_path: String::from("test/resources/start.bpmn")
+        });
+
+        let start_state = collaboration.create_start_state();
+
+        assert_eq!(start_state, State {
+            snapshots: vec![ProcessSnapshot::new(
+                String::from("process"),
+                vec![String::from("Flow_1"), String::from("Flow_2")])]
+        });
+    }
+
+    #[test]
+    fn try_execute_task() {
+        let collaboration = read_bpmn_file(&Config {
+            file_path: String::from("test/resources/task.bpmn")
+        });
+        let process = get_first_process(&collaboration);
+
+        let flow_node: &FlowNode = get_flow_node_with_id(process, String::from("Activity_A"));
+        let start_state = collaboration.create_start_state();
+
+        let next_states = flow_node.try_execute(
+            get_first_snapshot(&start_state),
+            &start_state);
+
+        assert_eq!(next_states, vec![
+            State::new(String::from("process"), vec![String::from("Flow_2"), String::from("Flow_3"), String::from("Flow_4")]),
+            State::new(String::from("process"), vec![String::from("Flow_1"), String::from("Flow_3"), String::from("Flow_4")]),
+        ])
+    }
+
+    #[test]
+    fn try_execute_exg_choice() {
+        let collaboration = read_bpmn_file(&Config {
+            file_path: String::from("test/resources/exg.bpmn")
+        });
+        let process = get_first_process(&collaboration);
+
+        let flow_node: &FlowNode = get_flow_node_with_id(process, String::from("Gateway_1"));
+        let start_state = collaboration.create_start_state();
+
+        let next_states = flow_node.try_execute(
+            get_first_snapshot(&start_state),
+            &start_state);
+
+        assert_eq!(next_states, vec![
+            State::new(String::from("process"), vec![String::from("Flow_2")]),
+            State::new(String::from("process"), vec![String::from("Flow_3")]),
+        ])
+    }
+
+    #[test]
+    fn try_execute_exg_merge() {
+        let collaboration = read_bpmn_file(&Config {
+            file_path: String::from("test/resources/exg.bpmn")
+        });
+        let process = get_first_process(&collaboration);
+
+        let flow_node: &FlowNode = get_flow_node_with_id(process, String::from("Gateway_2"));
+        let start_state = State::new(String::from("process"), vec![
+            String::from("Flow_2"),
+            String::from("Flow_3")],
+        );
+
+        let next_states = flow_node.try_execute(
+            get_first_snapshot(&start_state),
+            &start_state);
+
+        assert_eq!(next_states, vec![
+            State::new(String::from("process"), vec![String::from("Flow_3"), String::from("Flow_4")]),
+            State::new(String::from("process"), vec![String::from("Flow_2"), String::from("Flow_4")]),
+        ]);
+    }
+
+    #[test]
+    fn try_execute_pg_split() {
+        let collaboration = read_bpmn_file(&Config {
+            file_path: String::from("test/resources/pg.bpmn")
+        });
+        let process = get_first_process(&collaboration);
+
+        let flow_node: &FlowNode = get_flow_node_with_id(process, String::from("Gateway_1"));
+        let start_state = collaboration.create_start_state();
+
+        let next_states = flow_node.try_execute(
+            get_first_snapshot(&start_state),
+            &start_state);
+
+        assert_eq!(next_states, vec![
+            State::new(String::from("process"), vec![String::from("Flow_2"), String::from("Flow_3")]),
+        ])
+    }
+
+    #[test]
+    fn try_execute_pg_sync() {
+        let collaboration = read_bpmn_file(&Config {
+            file_path: String::from("test/resources/pg.bpmn")
+        });
+        let process = get_first_process(&collaboration);
+
+        let flow_node: &FlowNode = get_flow_node_with_id(process, String::from("Gateway_2"));
+        let start_state = State::new(String::from("process"), vec![
+            String::from("Flow_2"),
+            String::from("Flow_3")],
+        );
+
+        let next_states = flow_node.try_execute(
+            get_first_snapshot(&start_state),
+            &start_state);
+
+        assert_eq!(next_states, vec![
+            State::new(String::from("process"), vec![String::from("Flow_4")]),
+        ]);
+    }
+
+    fn get_first_process(collaboration: &BPMNCollaboration) -> &BPMNProcess {
+        let process = collaboration.participants.get(0).unwrap();
+        process
+    }
+
+    fn get_first_snapshot(start_state: &State) -> &ProcessSnapshot {
+        let snapshot = start_state.snapshots.get(0).unwrap();
+        snapshot
+    }
+
+    fn get_flow_node_with_id(process: &BPMNProcess, id: String) -> &FlowNode {
+        process.flow_nodes.iter().find(|flow_node| { flow_node.id == id }).unwrap()
+    }
+}
