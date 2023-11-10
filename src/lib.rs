6295c0b2ca8b4c1389bcdd5005885a3c3b27b811
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-11-10 09:25:11 +0100

    Add outputs to the console

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,55 @@ pub fn run(config: Config) -> Result<ModelCheckingResult, Box<dyn Error>> {
     let start = collaboration.create_start_state();
     let result: ModelCheckingResult = collaboration.explore_state_space(start, config.properties);
 
+    output_to_console(&result);
+    // TODO: Make it possible to export the state space to a file by a param (json).
+
+    Ok(result)
+}
+
+fn output_to_console(result: &ModelCheckingResult) {
+    output_state_information(result);
+    println!();
+    output_property_results(result);
+}
+
+fn output_property_results(result: &ModelCheckingResult) {
+    for property_result in result.property_results.iter() {
+        if property_result.fulfilled {
+            println!("The property {:?} is fulfilled.", property_result.property)
+        } else {
+            print!(
+                "The property {:?} is not fulfilled. ",
+                property_result.property
+            );
+            match property_result.property {
+                GeneralProperty::Safeness => {
+                    println!(
+                        "The sequence flow(s) {:?} can hold two or more tokens.",
+                        property_result.problematic_elements
+                    );
+                }
+                GeneralProperty::OptionToComplete => {
+                    println!();
+                }
+                GeneralProperty::ProperCompletion => {
+                    println!(
+                        "The end event(s) {:?} consume two or more tokens.",
+                        property_result.problematic_elements
+                    );
+                }
+                GeneralProperty::NoDeadActivities => {
+                    println!(
+                        "The activities {:?} cannot be executed.",
+                        property_result.problematic_elements
+                    );
+                }
+            }
+        }
+    }
+}
+
+fn output_state_information(result: &ModelCheckingResult) {
     println!("State space generation successful!");
     println!(
         "States: {}, Transitions: {}",
@@ -37,8 +86,4 @@ pub fn run(config: Config) -> Result<ModelCheckingResult, Box<dyn Error>> {
         "Terminated states: {}",
         result.state_space.terminated_state_hashes.len()
     );
-    // TODO: Output the property checking results.
-    // TODO: Make it possible to export the state space to a file by a param!.
-
-    Ok(result)
 }
