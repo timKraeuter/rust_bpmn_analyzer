900d2de617e70fa138e8605098f0a7d7eb5a0bd8
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-02-19 19:31:12 +0100

    Remove redundant config field in test

--- tests/state_space_generation.rs
+++ tests/state_space_generation.rs
@@ -10,7 +10,6 @@ fn test_stable_state_space1() {
             Property::ProperCompletion,
             Property::NoDeadActivities,
         ],
-        result_path: None,
     };
     let result = bpmnanalyzer::run(config).unwrap();
     assert_eq!(7, result.state_space.states.len());
@@ -22,7 +21,6 @@ fn test_stable_state_space2() {
     let config = Config {
         file_path: "tests/resources/integration/p6.bpmn".to_string(),
         properties: vec![],
-        result_path: None,
     };
     let result = bpmnanalyzer::run(config).unwrap();
     assert_eq!(134, result.state_space.states.len());
