ece9c1020c6857066cf4cb2acd042869ed69ec81
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-03-04 21:37:52 +0100

    Fix infinite recursion error

--- src/states/state_space.rs
+++ src/states/state_space.rs
@@ -24,29 +24,30 @@ impl StateSpace {
             .unwrap_or_else(|| panic!("State for {} not found!", state_hash))
     }
 
-    // TODO: This can loop and never end: We should remember visited states.
     pub fn get_path_to_state(&self, state_hash: u64) -> Option<Vec<(String, u64)>> {
-        self.get_path(self.start_state_hash, state_hash)
+        self.get_path(self.start_state_hash, state_hash, &mut HashMap::new())
     }
-    fn get_path(&self, from_state_hash: u64, to_state_hash: u64) -> Option<Vec<(String, u64)>> {
+    fn get_path(
+        &self,
+        from_state_hash: u64,
+        to_state_hash: u64,
+        seen_states: &mut HashMap<u64, bool>,
+    ) -> Option<Vec<(String, u64)>> {
         match self.transitions.get(&from_state_hash) {
             None => None,
             Some(next_states) => {
-                match next_states
-                    .iter()
-                    .find(|(_, next_state_hash)| next_state_hash == &to_state_hash)
-                {
-                    None => {}
-                    Some(last_transition) => {
-                        return Some(vec![last_transition.clone()]);
+                for (flow_node_id, next_state_hash) in next_states {
+                    if seen_states.contains_key(next_state_hash) {
+                        continue;
                     }
-                }
-                // Not found continue searching
-                for (flow_node_id, next_state) in next_states {
-                    match self.get_path(*next_state, to_state_hash) {
+                    if *next_state_hash == to_state_hash {
+                        return Some(vec![(flow_node_id.clone(), *next_state_hash)]);
+                    }
+                    seen_states.insert(*next_state_hash, true);
+                    match self.get_path(*next_state_hash, to_state_hash, seen_states) {
                         None => {}
                         Some(mut path) => {
-                            path.insert(0, (flow_node_id.clone(), *next_state));
+                            path.insert(0, (flow_node_id.clone(), *next_state_hash));
                             return Some(path);
                         }
                     };
